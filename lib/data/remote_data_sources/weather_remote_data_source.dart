70046e34ac24440407399632c1c32e1e97302f6f
Daria Maciejewska <daria.maciejewska9@gmail.com>
2022-03-10 19:56:14 +0100

    add Dio handling errors

--- lib/data/remote_data_sources/weather_remote_data_source.dart
+++ lib/data/remote_data_sources/weather_remote_data_source.dart
@@ -5,12 +5,12 @@ class WeatherRemoteDataSource {
     required String city,
   }) async {
     try {
-          final response = await Dio().get<Map<String, dynamic>>(
-        'http://api.weatherapi.com/v1/current.json?key=5d0ddf80d11347a4ac4174006220903 &q=$city&aqi=no');
-    return response.data;
-
+      final response = await Dio().get<Map<String, dynamic>>(
+          'http://api.weatherapi.com/v1/current.json?key=5d0ddf80d11347a4ac4174006220903 &q=$city&aqi=no');
+      return response.data;
     } on DioError catch (error) {
-      
+      throw Exception(
+          error.response?.data['error']['message'] ?? 'Unknow error');
     }
   }
 }
